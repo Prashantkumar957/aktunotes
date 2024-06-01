86fada09649bee5bcfb4d3c8c168e42486e14fa2
PrashantKumar957 <Prashantkumar.official957@gmail.com>
2024-06-01 12:32:30 +0530

    Initial commit

---
+++ app/src/main/java/com/prashantkumar957/aktunotes/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.prashantkumar957.aktunotes
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.prashantkumar957.aktunotes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+
+        }
+        binding.skipBtn.setOnClickListener {
+            startActivity(Intent(this, DashboardUserActivity::class.java))
+
+
+        }
+    }
+
+}
